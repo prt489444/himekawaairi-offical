1804160435c8085eb2cf6740167e68af52d2e2e7
prt489444 <41227566+prt489444@users.noreply.github.com>
2026-06-25 22:37:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
     mobileMenu.innerHTML = `
       <div class="px-6 flex flex-col gap-4 text-center text-base font-medium text-gray-700">
         <a href="#profile" class="py-3 hover:text-pink-500 mobile-link">プロフィール</a>
-        <a href="#career" class="py-3 hover:text-pink-500 mobile-link">出演作</a>
         <a href="#voice" class="py-3 hover:text-pink-500 mobile-link">ボイスサンプル</a>
+        <a href="#career" class="py-3 hover:text-pink-500 mobile-link">出演作</a>
         <a href="#sns" class="py-3 hover:text-pink-500 mobile-link">SNS</a>
         <a href="#contact" class="py-3 hover:text-pink-500 mobile-link">お問い合わせ</a>
       </div>
